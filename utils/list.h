0afd8d9326cae99001483ebdf3372002f504fe56
alexlexx <alexlexx1@gmail.com>
2017-05-30 01:43:35 +0300

    add list

---
+++ utils/list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+	void ftoa(double n, char *res, int afterpoint);
+
+	typedef struct List_t {
+		List_t * next;
+		List_t * prev;
+	} List;
+
+	//init list head
+	#define INIT_LIST(list)\
+		list->next = list;\
+		list->prev = list;
+
+	//add list item to tail of list
+	void list_add_tail(List * head, List * item);
+
+	//remove lit item from list
+	void list_remove(List * item);
+
+#endif
--- utils/utils.h
+++ utils/utils.h
@@ -1,4 +1,21 @@
 #ifndef UTILS_H
 #define UTILS_H
     void ftoa(double n, char *res, int afterpoint);
+
+    typedef struct List_t {
+        List_t * next;
+        List_t * prev;
+    } List;
+
+    //init list head
+    #define INIT_LIST(list)\
+        list->next = list;\
+        list->prev = list;
+
+    //add list item to tail of list
+    void list_add_tail(List * head, List * item);
+
+    //remove lit item from list
+    void list_remove(List * item);
+
 #endif
