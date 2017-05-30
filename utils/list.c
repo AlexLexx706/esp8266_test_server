6013aeaf3895811c1cab180e1cc72dd2c9384a80
alexlexx <alexlexx1@gmail.com>
2017-05-30 03:04:08 +0300

    git add list test

--- utils/list.c
+++ utils/list.c
@@ -1,1 +1,62 @@
 #include "list.h"
+#include "assert.h"
+
+#ifdef TEST
+    #include <stdio.h>
+#endif
+
+
+void list_add_tail(List * head, List * item) {
+    assert(head);
+    assert(item);
+    item->next = head;
+    item->prev = head->prev;
+    head->prev->next = item;
+    head->prev = item;
+    #ifdef TEST
+    fprintf(stderr, "h:%p h->p:%p h->n:%p i->p:%p i->n:%p\n", head, head->prev, head->next, item->prev, item->next);
+    #endif
+}
+
+//remove lit item from list
+int list_remove(List * item) {
+    assert(item);
+    if (item->next != item) {
+        item->prev->next = item->next;
+        item->next->prev = item->prev;
+        return 0;
+    }
+    return 1;
+}
+
+
+#ifdef TEST
+
+void main() {
+    List head;
+    INIT_LIST(&head);
+    List item1;
+    List item2;
+    List item3;
+    list_add_tail(&head, &item1);
+    list_add_tail(&head, &item2);
+    list_add_tail(&head, &item3);
+
+    List * pos;
+    int i = 0;
+    for (pos = (&head)->next; pos != (&head); pos = pos->next) {
+        i++;
+    }
+    fprintf(stderr, "count:%d\n", i);
+
+    list_remove(&item2);
+    
+    i = 0;
+    for (pos = (&head)->next; pos != (&head); pos = pos->next) {
+        i++;
+    }
+    fprintf(stderr, "count:%d\n", i);
+
+}
+
+#endif
--- utils/list.h
+++ utils/list.h
@@ -3,19 +3,19 @@
 	void ftoa(double n, char *res, int afterpoint);
 
 	typedef struct List_t {
-		List_t * next;
-		List_t * prev;
+		struct List_t * next;
+		struct List_t * prev;
 	} List;
 
 	//init list head
-	#define INIT_LIST(list)\
-		list->next = list;\
-		list->prev = list;
+	#define INIT_LIST(head)\
+		(head)->next = (head);\
+		(head)->prev = (head);
 
 	//add list item to tail of list
 	void list_add_tail(List * head, List * item);
 
 	//remove lit item from list
-	void list_remove(List * item);
+	int list_remove(List * item);
 
 #endif
