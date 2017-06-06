392f8a37ae9d8c641cf4574e533c4690815dfa5b
AlexLexx <alexlexx1@gmail.com>
2017-06-06 20:10:57 +0300

    Update list.h

--- utils/list.h
+++ utils/list.h
@@ -1,6 +1,7 @@
 /** alexlexx 2017 */
 #ifndef LIST_H
 #define LIST_H
+	/**two directions list definition*/
 	typedef struct List_t {
 		struct List_t * next;
 		struct List_t * prev;
@@ -13,8 +14,10 @@
 
 	//return pointr to item 
 	#define LIST_ITEM(ItemType, head_member, pos) ((ItemType *)((char *)pos - (char *)(&((ItemType *)0)->head_member)))
-
+	
+	//define for head
 	#define LIST_ITER(head) for (pos = (head)->next; pos != (head); pos = pos->next)
+
 	//add list item to tail of list
 	void list_add_tail(List * head, List * item);
 
